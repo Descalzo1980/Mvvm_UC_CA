7ffd4316e3f7d04a809c87c4cea85c3c45f85b7b
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-10-16 19:49:01 +0300

    add module

--- app/src/main/java/com/example/mvvmucca/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mvvmucca/presentation/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.mvvmucca.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mvvmucca.repository.UserRepositoryImpl
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mvvmucca.databinding.ActivityMainBinding
+import com.example.mvvmucca.repository.UserRepositoryImpl
 import com.example.mvvmucca.repository.storage.sharedprefs.SharedPrefUserStorage
 import domain.usecase.GetUserNameUseCase
 import domain.usecase.SaveUserNameUseCase
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-        UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = applicationContext)) }
+        UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = applicationContext))
+    }
     private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
         GetUserNameUseCase(
             userRepository = userRepository
@@ -36,14 +37,14 @@ class MainActivity : AppCompatActivity() {
         val sendButton = binding.sendButton
         val receiveButton = binding.receiveButton
 
-        sendButton.setOnClickListener{
+        sendButton.setOnClickListener {
             val text = dataEditText.text.toString()
             val params = domain.models.SaveUserNameParam(name = text)
-            val result : Boolean = saveUserNameUseCase.execute(param = params)
+            val result: Boolean = saveUserNameUseCase.execute(param = params)
             dataTextView.text = "Save result = $result"
         }
-        receiveButton.setOnClickListener{
-            val userName : domain.models.UserName = getUserNameUseCase.execute()
+        receiveButton.setOnClickListener {
+            val userName: domain.models.UserName = getUserNameUseCase.execute()
             dataTextView.text = "${userName.firstName} ${userName.lastName}"
         }
     }
