348547a2dc3e2bbdcb529113c85aa7910951ac0c
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-10-16 21:07:19 +0300

    add observer

--- app/src/main/java/com/example/mvvmucca/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mvvmucca/presentation/MainActivity.kt
@@ -3,32 +3,15 @@ package com.example.mvvmucca.presentation
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import com.example.mvvmucca.databinding.ActivityMainBinding
-import com.example.mvvmucca.repository.UserRepositoryImpl
-import com.example.mvvmucca.repository.storage.sharedprefs.SharedPrefUserStorage
-import domain.usecase.GetUserNameUseCase
-import domain.usecase.SaveUserNameUseCase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-        UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = applicationContext))
-    }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        GetUserNameUseCase(
-            userRepository = userRepository
-        )
-    }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        SaveUserNameUseCase(
-            userRepository = userRepository
-        )
-    }
+
     private lateinit var viewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,22 +21,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         Log.e("AAA", "Activity create")
 
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel = ViewModelProvider(this,MainViewModelFactory(this))[MainViewModel::class.java]
 
         val dataTextView = binding.dataTextView
         val dataEditText = binding.dataEditView
         val sendButton = binding.sendButton
         val receiveButton = binding.receiveButton
 
+        viewModel.resultLive.observe(this, Observer{
+            dataTextView.text = it
+        })
+
         sendButton.setOnClickListener {
             val text = dataEditText.text.toString()
-            val params = domain.models.SaveUserNameParam(name = text)
-            val result: Boolean = saveUserNameUseCase.execute(param = params)
-            dataTextView.text = "Save result = $result"
+            viewModel.save(text)
         }
         receiveButton.setOnClickListener {
-            val userName: domain.models.UserName = getUserNameUseCase.execute()
-            dataTextView.text = "${userName.firstName} ${userName.lastName}"
+            viewModel.load()
         }
     }
 }
--- app/src/main/java/com/example/mvvmucca/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmucca/presentation/MainViewModel.kt
@@ -1,9 +1,20 @@
 package com.example.mvvmucca.presentation
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mvvmucca.repository.UserRepositoryImpl
+import com.example.mvvmucca.repository.storage.sharedprefs.SharedPrefUserStorage
+import domain.usecase.GetUserNameUseCase
+import domain.usecase.SaveUserNameUseCase
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val getUserNameUseCase : GetUserNameUseCase,
+    private val saveUserNameUseCase : SaveUserNameUseCase
+) : ViewModel() {
+
+    var resultLive = MutableLiveData<String>()
+
 
     init {
         Log.e("AAA", "VM create")
@@ -14,4 +25,14 @@ class MainViewModel : ViewModel() {
         super.onCleared()
     }
 
+    fun save(text : String){
+        val params = domain.models.SaveUserNameParam(name = text)
+        val resultData: Boolean = saveUserNameUseCase.execute(param = params)
+        resultLive.value = "Save result = $resultData"
+    }
+
+    fun load(){
+        val userName: domain.models.UserName = getUserNameUseCase.execute()
+        resultLive.value = "${userName.firstName} ${userName.lastName}"
+    }
 }
